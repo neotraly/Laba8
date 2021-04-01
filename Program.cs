d845f44ff1ef76f0b9ebad7089fffd1760cdd197
neotraly <denis.zylev2016@gmail.com>
2021-04-01 14:00:55 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text.RegularExpressions;
+
+namespace Laba8
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Number: ");
+            string number = Console.ReadLine();
+            if (number.Length == 4)
+                Console.WriteLine(number);
+            else
+            {
+                var res = Regex.Replace(number, ".{4}", "$0-");
+                Console.WriteLine(res);
+            }
+            
+
+        }
+    }
+}
